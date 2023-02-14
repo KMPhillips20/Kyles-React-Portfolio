e5b9e9edc63874df59277e1644c4f4359d40b0ca
Kyle <kylemphillips65@yahoo.com>
2023-02-13 22:44:37 -0500

    added home, about, portfolio, and contact to the navtab

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -3,19 +3,55 @@ import React from 'react';
 function NavTabs({ currentPage, handlePageChange, pages }) {
   // Opportunity
   return (
+
+
     <ul className="nav nav-tabs">
-      {pages.map(page => (
-        <li key={page} className="nav-item">
-          <a
-            href={`#${page.toLowerCase()}`}
-            onClick={() => handlePageChange(page)}
-            className={currentPage === page ? 'nav-link active' : 'nav-link'}
-          >
-            {page}
-          </a>
+      <li className="nav-item">
+        <a
+          href="#home"
+          onClick={() => handlePageChange('Home')}
+          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+        >
+          Home
+        </a>
       </li>
-      ))}
-    </ul>
+
+
+
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+
+
+
+      <li className="nav-item">
+        <a
+          href="portfolio"
+          onClick={() => handlePageChange('Portfolio')}
+          className={currentPage === Portfolio ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+        </a>
+      </li>
+
+
+
+      <li className="nav-item">
+        <a
+          href="contact"
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+    </ul >
   );
 }
 
