c48848ad7215b03c4fb7ed2c89453f1d6645e9ee
Kyle <kylemphillips65@yahoo.com>
2023-02-13 21:03:06 -0500

    added folders and files to my components

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 import "bootswatch/dist/morph/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
