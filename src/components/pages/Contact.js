e2f2e8e9b49396b3e1b275eade1a6a32f07833a0
Kyle <kylemphillips65@yahoo.com>
2023-02-13 23:09:16 -0500

    added github and linkedin to the contact js

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-function Contact () {
+export default function Contact () {
 
   return (
     <div>
       <h1>Contact</h1>
+      <p>
+        GitHub:
+        LinkedIn: 
+      </p>
     </div>
-  )
+  );
 }
 
-export default Contact
