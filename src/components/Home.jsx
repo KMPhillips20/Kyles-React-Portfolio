007bf431437b3a2bf0155a97754331af50362166
Kyle <kylemphillips65@yahoo.com>
2023-02-14 15:51:02 -0500

    added to the home jsx file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,3 +2,23 @@ import React, { useReact} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import pageComponents from "./pages/index";
+
+const pages = Object.keys(pageComponents);
+
+export default function Homepage() {
+    const [currentPage, setCurrentPage] = useState("About");
+
+    const Page = () => pageComponents[currentPage]();
+    const handlePageChange = (page) => setCurrentPage(page);
+    return (
+      <>
+        <Header
+          handlePageChange={handlePageChange}
+          pages={pages}
+          currentPage={currentPage}
+        />
+        <Page className="m-3 p-2 d-flex container min-vh-100" />
+        <Footer />
+      </>
+    );
+}
