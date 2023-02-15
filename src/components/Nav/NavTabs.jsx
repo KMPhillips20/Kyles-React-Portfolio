5606aef633bd78c5e78217d884ef172d40b548f8
Kyle <kylemphillips65@yahoo.com>
2023-02-15 14:51:11 -0500

    changed files to jsx and trying to get things to display

--- src/components/Nav/NavTabs.jsx
+++ src/components/Nav/NavTabs.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function NavTabs({ currentPage, handlePageChange, pages }) {
-  // Opportunity
+
   return (
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import "bootswatch/dist/morph/bootstrap.min.css";
+// import "bootswatch/dist/morph/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
