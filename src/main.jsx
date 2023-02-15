2dad5977b26d4e4e179ead0dcf2539535e73708d
Kyle <kylemphillips65@yahoo.com>
2023-02-15 15:30:21 -0500

    had to fix the main jsx becasue I have  version 18 and not 17

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-// import "bootswatch/dist/morph/bootstrap.min.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+const container = document.getElementById("root");
+
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App  />);
